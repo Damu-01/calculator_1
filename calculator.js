5376684b9fcfb16b7b5ae37dddb6514f8244383f
Damu-01 <126450504+Damu-01@users.noreply.github.com>
2023-03-07 20:37:01 +0530

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -5,14 +5,23 @@ var inputFeild = document.querySelector('.input-2');
 function number(digit) {
     // alert(digit);
     // var x= document.getElementById('result').value;
-    // document.getElementById('result').value= x+ digit;
+    // document.getElementById('result').value= x+digit;
 
-    if (operator.value == '0') {
-        operator.value = digit;
- 
+    // if (operator.value == '0') {
+    //     operator.value = digit;
+
+    // }
+    // else {
+    //     operator.value += digit;
+    // }
+
+    
+    if (inputFeild.value == '0') {
+        inputFeild.value = digit;
+
     }
     else {
-        operator.value += digit;
+        inputFeild.value += digit;
     }
 
 }
@@ -28,23 +37,55 @@ function clearResult() {
 function delet() {
     // alert('hi');
 
-    del = operator.value;
-    if (del.value != '0') {
+    // del = operator.value
 
-        operator.value = del.slice(0, -1);
+    // if (del.length != 1) {
+
+    //     operator.value = del.slice(0, -1);
+    // }
+    // else {
+    //     operator.value = '0';
+    // }
+
+    del = inputFeild.value
+
+    if (del.length != 1) {
+
+        inputFeild.value = del.slice(0, -1);
     }
     else {
-        operator.value = '0'
+        inputFeild.value = '0';
     }
 
 }
 
 function sum() {
 
-    solve = operator.value;
-    operator.value = eval(solve);
-    inputFeild.value = solve + ' =';
+    solve = inputFeild.value;
+    // operator.value = eval(solve);
+    // inputFeild.value = solve + ' =';
+
+    inputFeild.value = eval(solve);
+    operator.value = solve + '=';
+
+    // let his = document.getElementById('history');
+    // his.innerHTML=sovle+'='+ solve +'<br>';
+    // console.log(his);
 }
+
+function cal_history() {
+    let his = document.getElementById('history');
+    his.innerHTML += inputFeild.value + '<br>' + solve + '=' + '<br>';
+
+
+}
+
+// function CalHis() {
+//     // alert('hii')
+//     document.getElementById('history1 history').value = '0';
+
+// }
+
 
 
 
